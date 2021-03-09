c19d12b89bc6b281bff9abf0ddfbb46e21c68bc3
Joanle <61175299+Joanle@users.noreply.github.com>
2021-03-09 00:19:41 -0600

    Update HW_Ambient_Sensor.h

--- Reap_Eat_V3/HARDWARE/HW_AMBIENT_SENSOR/HW_Ambient_Sensor.h
+++ Reap_Eat_V3/HARDWARE/HW_AMBIENT_SENSOR/HW_Ambient_Sensor.h
@@ -8,6 +8,44 @@
 #ifndef HARDWARE_HW_AMBIENT_H_
 #define HARDWARE_HW_AMBIENT_H_
 
+#include "DRV_I2c.h"
+#include "HW_Debug_Com.h"
+
+//#define BME280_32BIT_ENABLE
+#include "bme280.h"
+
+struct identifier
+{
+    /* Variable to hold device address */
+    uint8_t dev_addr;
+
+    /* Variable that contains file descriptor */
+    int8_t fd;
+};
+
+struct bme280_dev dev;
+struct identifier id;
+
+struct bme280_data comp_data;
+
+uint32_t req_delay;
+
+//extern uint8_t ambient_sensor_initialized = 1;
+
+void HW_Ambient_Sensor_Init(void);
+
+int32_t HW_Ambient_Sensor_Get_Temp(void);
+
+uint8_t HW_Ambient_Sensor_Get_Humd(void);
+
+void HW_Ambient_Sensor_Get_Data(void);
+
+int8_t HW_Ambient_Sensor_I2C_Write(uint8_t reg_addr, uint8_t *reg_data, uint32_t len, void *intf_ptr);
+
+int8_t HW_Ambient_Sensor_I2C_Read(uint8_t reg_addr, uint8_t *reg_data, uint32_t len, void *intf_ptr);
+
+void HW_Ambient_Sensor_I2C_Delay(uint32_t period, void *intf_ptr);
+
 
 
 #endif /* HARDWARE_HW_AMBIENT_H_ */
