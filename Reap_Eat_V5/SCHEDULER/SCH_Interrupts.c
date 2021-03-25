c40c38b39b3a4d137c2738ebefa14360da1ced03
Joanle <61175299+Joanle@users.noreply.github.com>
2021-03-25 01:19:00 -0500

    Update SCH_Interrupts.c

--- Reap_Eat_V5/SCHEDULER/SCH_Interrupts.c
+++ Reap_Eat_V5/SCHEDULER/SCH_Interrupts.c
@@ -9,10 +9,11 @@
 
 void SCH_Interrupt_Uart0(void)
 {
-
+    UARTIntClear(UART0_BASE, UART_INT_RX | UART_INT_RT);
+    Semaphore_post(semaphore0);
 }
 
 void SCH_Interrupt_Uart1(void)
 {
-
+    UARTIntClear(UART1_BASE, UART_INT_RX | UART_INT_RT);
 }
